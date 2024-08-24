04a1e4a3a670221c94c865b44c5217017f55f953
Vikas Valechha <vikas.valechha@gmail.com>
2024-08-24 13:05:08 +0530

    Add file-upload feature

---
+++ app/modules/FileMeta/Schema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const mongoose_delete = require('mongoose-delete')
+const { Schema } = mongoose;
+
+const fileSchema = new Schema({
+    urlId: { type: Schema.Types.ObjectId, ref: 'urlSchema' },
+    assetId: { type: String },
+    publicId: { type: String },
+    format: { type: String },
+    size: { type: Number, default: 0 }
+}, {
+    timestamps: true
+})
+
+fileSchema.index({ urlId: 1 })
+fileSchema.index({ publicId: 1 })
+fileSchema.index({ format: 1 })
+fileSchema.index({ size: 1 })
+fileSchema.plugin(mongoose_delete, { deletedAt: true, validateBeforeDelete: false, indexFields: ['deleted'], overrideMethods: true })
+
+const FileSchema = mongoose.model('FileSchema', fileSchema);
+module.exports = { FileSchema }
